0d212977b28d9a0f5457cc3274db2de179971482
Reece Fritz <reecefritz7@gmail.com>
2022-05-02 14:08:14 -0500

    Created Particles for GV

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public ParticleSystem goldenParticles;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            goldenParticles.Play();
+        }
+    }
+}

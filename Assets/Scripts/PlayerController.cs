4f67ecd3715c71c2ad3b45391b685983ddae2040
Reece Fritz <reecefritz7@gmail.com>
2022-05-02 14:27:16 -0500

    Added Particles for after GV

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,20 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
     public ParticleSystem goldenParticles;
+    public ParticleSystem afterGV;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             goldenParticles.Play();
+            //Play move animation
+            //turn on Golden Circle
+            //turn on Strength Power Up Sign
+            //turn on Defense Power Up Sign
+            //wait a little
+            //turn off Golden Circle
+            afterGV.Play();
+            //wait for a little bit
         }
     }
 }
